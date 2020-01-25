27a64e9906ea0950418347b1d4d1a0f04e2ef3a5
Vasyl Ilnytskyy <50253042+vilnytskyy@users.noreply.github.com>
2020-01-25 00:42:18 -0500

    Base game assets
    Learning how to build a game

---
+++ Assets/Scenes/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerPhysics))]
+
+public class PlayerController : MonoBehaviour
+{
+    //Player Handling
+
+    public float gravity = 20;
+    public float speed = 8;
+    public float acceleration = 30;
+    public float jumpHeight = 12;
+
+    private float currentSpeed;
+    private float targetSpeed;
+
+    private Vector2 amountToMove;
+
+    private PlayerPhysics playerPhysics;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPhysics = GetComponent<PlayerPhysics>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        targetSpeed = Input.GetAxisRaw("Horizontal") * speed;
+        currentSpeed = IncrementTowards(currentSpeed, targetSpeed, acceleration);
+
+        if (playerPhysics.grounded)
+        {
+
+            amountToMove.y = 0;
+
+            //Jump
+            if (Input.GetButtonDown("Jump"))
+            {
+                amountToMove.y = jumpHeight;
+            }
+        }
+
+        amountToMove.x = currentSpeed;
+        amountToMove.y -= gravity * Time.deltaTime;
+
+        playerPhysics.Move(amountToMove * Time.deltaTime);
+    }
+
+    private float IncrementTowards(float n, float target, float a)
+    {
+        if(n == target)
+        {
+            return n;
+        }
+        else
+        {
+            float dir = Mathf.Sign(target - n);
+            n += a * Time.deltaTime * dir;
+            return (dir == Mathf.Sign(target - n)) ? n : target;
+        }
+    }
+}
