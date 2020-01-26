d7fd6cf00d09fdaccbca0d5ae02e2c9d295c1cf0
Arthur Mouradian <arthurdmouradian@gmail.com>
2020-01-25 22:41:58 -0500

    Added functional sidebar

--- Web/911calls/src/App.js
+++ Web/911calls/src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
 
+//Arthur's sideNav
+import Toolbar from './components/Toolbar/Toolbar';
+import SideDrawer from './components/SideDrawer/SideDrawer';
+import Backdrop from './components/Backdrop/Backdrop'
+//
+
 const mapStyles = {
     width: '100%',
     height: '100%',
@@ -9,10 +15,22 @@ const mapStyles = {
 
 class MapContainer extends React.Component {
     state = {
+        sideDrawerOpen: false,
         showingInfoWindow: false,
         activeMarker: {},
         selectedPlace: {}
       };
+
+    //Side Navigation Code
+    drawerToggleClickHandler = () => {
+        this.setState((prevState) => {
+            return { sideDrawerOpen: !prevState.sideDrawerOpen };
+        });
+    }
+    backdropClickHandler = () => {
+        this.setState({ sideDrawerOpen: false });
+    };
+    //End Side Navigation Code
     
     onMarkerClick = (props, marker, e) =>{
         this.setState({
@@ -60,23 +78,37 @@ class MapContainer extends React.Component {
     
 
     render() {
+        // Side Navigation code
+        let backdrop;
+        if (this.state.sideDrawerOpen) {
+            backdrop = <Backdrop click={this.backdropClickHandler} />;
+        }
+        // End Side Navigation code
         return (
-            <Map
-                google={this.props.google}
-                zoom={12}
-                style={mapStyles}
-                initialCenter={{ lat: 40.7, lng: -73.9 }} //increase lat, moves up increase lng moves right
-            >
-                <Marker onClick={this.onMarkerClick} name= {'My House'} position={{ lat: 40.7, lng: -73.9}} /> 
-                
-                <InfoWindow
-                    marker={this.state.activeMarker}
-                    visible={this.state.showingInfoWindow}
-                    onClose={this.onClose}
-                >
-                    <div><h4>{this.state.selectedPlace.name}</h4></div>
-               </InfoWindow>
-            </Map>
+            //Side Nav code
+            <div style={{ height: '100%' }}>
+                <Toolbar drawerClickHandler={this.drawerToggleClickHandler} />
+                <SideDrawer show={this.state.sideDrawerOpen} /> ;
+                {backdrop}
+                <main style={{ marginTop: '31px' }}>
+                    <Map
+                        google={this.props.google}
+                        zoom={12}
+                        style={mapStyles}
+                        initialCenter={{ lat: 40.7, lng: -73.9 }} //increase lat, moves up increase lng moves right
+                        >
+                        <Marker onClick={this.onMarkerClick} name= {'My House'} position={{ lat: 40.7, lng: -73.9}} /> 
+                        
+                        <InfoWindow
+                            marker={this.state.activeMarker}
+                            visible={this.state.showingInfoWindow}
+                            onClose={this.onClose}
+                            >
+                            <div><h4>{this.state.selectedPlace.name}</h4></div>
+                    </InfoWindow>
+                    </Map>
+                </main>
+            </div>
         );
     }
 }
