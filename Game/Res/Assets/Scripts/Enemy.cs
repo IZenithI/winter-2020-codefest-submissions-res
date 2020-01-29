f9dcd795a04b058707715012eb639ce0b87b4b66
Brian Guo <brianguo810@gmail.com>
2020-01-28 20:36:14 -0500

    Added Enemy Script & Implemented Player Death on Enemy Contact

---
+++ Game/Res/Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name == "Player" || collision.name == "Player(Clone)")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+}
