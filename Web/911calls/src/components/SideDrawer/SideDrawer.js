f970541a11816f40fd95c2a9214c23dcc46df33e
Howard Jiang <hjiang4332@gmail.com>
2020-01-30 18:39:48 -0500

    Added killing enemy functionality

--- Web/911calls/src/components/SideDrawer/SideDrawer.js
+++ Web/911calls/src/components/SideDrawer/SideDrawer.js
@@ -9,14 +9,20 @@ const sideDrawer = props => {
    return (
         <nav className={drawerClasses}>
             <ul>
-                <a onClick = {() => props.toggleMap()}>Map</a>
+                <div onClick = {() => props.toggleMap()}>Map</div>
                 <select class="ui dropdown">
                     <option value="" onClick = {() => props.changeMapDefault()}> Default (everything) </option>
                     <option value="1" onClick = {() => props.changeMapMisdemeanor()}> Misdemeanor (small) </option>
                     <option value="2" onClick = {() => props.changeMapFelony()}> Felony (medium) </option>
                     <option value="3" onClick = {() => props.changeMapViolation()}> Violation (large) </option>
+                    <option value="4" onClick = {() => props.changeMapQueens()}> Queens </option>
+                    <option value="5" onClick = {() => props.changeMapManhattan()}> Manhattan </option>
+                    <option value="6" onClick = {() => props.changeMapBrooklyn()}> Brooklyn </option>
+                    <option value="7" onClick = {() => props.changeMapBronx()}> Bronx </option>
+                    <option value="8" onClick = {() => props.changeMapStatenIsland()}> Staten Island </option>
+
                 </select>
-                <a onClick = {() => props.toggleAboutMe()}>Meet The Team</a>
+                <div onClick = {() => props.toggleAboutMe()}>Meet The Team</div>
             </ul>
         </nav>
     );
