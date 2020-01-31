a141c530161b6b811d409472d480ffabd82e6d77
Brian Guo <brianguo810@gmail.com>
2020-01-31 02:26:37 -0500

    Implemented Markers when Zoomed In

--- iOS/ResProject/Controllers/MapController.swift
+++ iOS/ResProject/Controllers/MapController.swift
@@ -12,12 +12,18 @@ import GoogleMaps
 class MapController: UIViewController, GMSMapViewDelegate {
     private var mapView: GMSMapView!
     private var heatmapLayer: GMUHeatmapTileLayer!
+    private var marker: GMSMarker!
     private var button: UIButton!
     private var zoomInButton: UIButton!
     private var zoomOutButton: UIButton!
     
+    private var latArray: [Double] = []
+    private var lngArray: [Double] = []
+    
     private var gradientColors = [UIColor.green, UIColor.red]
     private var gradientStartPoints = [0.2, 1.0] as [NSNumber]
+    
+    var markerEnabled = false;
     
     var delegate: MapControllerDelegate?
     
@@ -28,6 +34,7 @@ class MapController: UIViewController, GMSMapViewDelegate {
         mapView.delegate = self
         self.view = mapView
 
+        makeMarkerLevelButton()
         makeZoomInButton()
         makeZoomOutButton()
         
@@ -38,6 +45,7 @@ class MapController: UIViewController, GMSMapViewDelegate {
         heatmapLayer.gradient = GMUGradient(colors: gradientColors,
                                             startPoints: gradientStartPoints,
                                             colorMapSize: 256)
+        parseJSON()
         addHeatmap()
         
         // Set the heatmap to the mapview.
@@ -60,8 +68,8 @@ class MapController: UIViewController, GMSMapViewDelegate {
     }
     
     // Parse JSON data and add it to the heatmap layer.
-    func addHeatmap()  {
-        var list = [GMUWeightedLatLng]()
+    // Parse JSON data
+    func parseJSON() {
         do {
             if let path = Bundle.main.url(forResource: "nycRecentComplaintsData", withExtension: "json", subdirectory: "Data") {
                 let data = try Data(contentsOf: path)
@@ -71,13 +79,13 @@ class MapController: UIViewController, GMSMapViewDelegate {
                         var lat : Double = 0.0
                         var lng : Double = 0.0
                         if let latString = item["Latitude"]{
-                             lat = Double(latString) ?? 0.0
+                            lat = Double(latString) ?? 0.0
+                            latArray.append(lat);
                         }
-                        if let lngString = item["Longitude"] {
-                             lng = Double(lngString) ?? 0.0
+                        if let lngString = item["Longitude"]{
+                            lng = Double(lngString) ?? 0.0
+                            lngArray.append(lng);
                         }
-                        let coords = GMUWeightedLatLng(coordinate: CLLocationCoordinate2DMake(lat , lng), intensity: 2.0)
-                        list.append(coords)
                     }
                 } else {
                     print("Could not read the JSON.")
@@ -86,6 +94,15 @@ class MapController: UIViewController, GMSMapViewDelegate {
         } catch {
             print(error.localizedDescription)
         }
+    }
+    
+    //add it to the heatmap layer.
+    func addHeatmap()  {
+        var list = [GMUWeightedLatLng]()
+        for (index, _) in latArray.enumerated() {
+            let coords = GMUWeightedLatLng(coordinate: CLLocationCoordinate2DMake(latArray[index] , lngArray[index]), intensity: 2.0)
+            list.append(coords)
+        }
         // Add the latlngs to the heatmap layer.
         heatmapLayer.weightedData = list
     }
@@ -93,6 +110,15 @@ class MapController: UIViewController, GMSMapViewDelegate {
     
     func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
         print("You tapped at \(coordinate.latitude), \(coordinate.longitude)")
+    }
+    
+    func makeMarkerLevelButton() {
+        zoomInButton = UIButton(frame: CGRect(x: 330, y: 730, width: 35, height: 35))
+        zoomInButton.backgroundColor = .white
+        zoomInButton.setTitleColor(UIColor.black, for: .normal)
+        zoomInButton.setTitle("*", for: .normal)
+        zoomInButton.addTarget(self, action: #selector(zoomToMarker), for: .touchUpInside)
+        self.mapView.addSubview((zoomInButton))
     }
     
     func makeZoomInButton() {
@@ -114,13 +140,39 @@ class MapController: UIViewController, GMSMapViewDelegate {
     }
     
     @objc
+    func zoomToMarker() {
+        mapView.animate(with: GMSCameraUpdate.zoom(to: 15))
+        if(mapView.camera.zoom >= 10 && markerEnabled == false){
+            for (index, _) in latArray.enumerated() {
+                let position = CLLocationCoordinate2D(latitude: latArray[index], longitude: lngArray[index])
+                marker = GMSMarker(position: position)
+                marker.map=mapView
+                markerEnabled = true;
+            }
+        }
+    }
+    
+    @objc
     func zoomIn() {
-        mapView.animate(with: GMSCameraUpdate.zoom(by: 0.5))
+        mapView.animate(with: GMSCameraUpdate.zoom(by: 1))
+        if(mapView.camera.zoom >= 10 && markerEnabled == false){
+            for (index, _) in latArray.enumerated() {
+                let position = CLLocationCoordinate2D(latitude: latArray[index], longitude: lngArray[index])
+                marker = GMSMarker(position: position)
+                marker.map=mapView
+                markerEnabled = true;
+            }
+        }
     }
     
     @objc
     func zoomOut() {
-        mapView.animate(with: GMSCameraUpdate.zoom(by: -0.5))
+        mapView.animate(with: GMSCameraUpdate.zoom(by: -1))
+        if(mapView.camera.zoom <= 10 && markerEnabled == true){
+            mapView.clear()
+            markerEnabled = false;
+            addHeatmap()
+        }
     }
 }
 
